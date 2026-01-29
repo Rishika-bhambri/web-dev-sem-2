010312627a1d73e2b7ba8e0f364251302e3781d2
Rishika-bhambri <bhambririshika@gmail.com>
2026-01-29 20:14:39 +0530

    Add files via upload

---
+++ js-day-4/script.js
@@ -0,0 +1,10 @@
+let user={
+    FullName:"Rishika",
+    address:"Delhi, India",
+    mobileno:"8976543210",
+    favcolor:["Red","Blue","Green"], 
+}
+console.log(user)
+ 
+//we can acess the individual value by targeting key with variable name
+// console.log(user.fullname,user.address,user.numner,user.favcolour[1]);
